480f4e621567f57197589aa24139518a05a7d81f
IkerromoMontilivi <a2409865@institutmontilivi.cat>
2024-10-28 14:22:14 +0100

    ex07 fet classe

---
+++ ex07/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ex07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, numMajor, numMenor;
+
+            Console.Write("Escriu un numero: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+            numMajor = numero;
+            numMenor = numero;
+
+            while(numero != 0)
+            {
+                if (numero > numMajor)
+                    numMajor = numero;
+                else if (numero < numMenor)
+                    numMenor = numero;
+
+                Console.Write("Escriu un altre numero: ");
+                numero = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine($"El valor mes gran es {numMajor} i el mes petit es {numMenor}");
+
+        }
+    }
+}
