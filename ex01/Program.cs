be9c9135e5a9be0636ca484da1311ddcd3533bd0
IkerromoMontilivi <a2409865@institutmontilivi.cat>
2024-10-28 13:53:48 +0100

    ex01 acabat

--- ex01/Program.cs
+++ ex01/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hellasdfasdfwaegewaed!");
+            //Declaracio de variables
+            int numero, cont;
+
+            //Introduccio de valors
+            Console.Write("Introduiex un numero enter: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+            cont = 1;
+
+            //Algorisme / calcul
+            while (numero != 0)
+            {
+                Console.Write("Introdueix un altre valor: ");
+                numero = Convert.ToInt32(Console.ReadLine());
+                cont++;
+            }
+            //Mostrar resultats
+            Console.WriteLine($"Has introduit {cont} valors");
         }
     }
 }
