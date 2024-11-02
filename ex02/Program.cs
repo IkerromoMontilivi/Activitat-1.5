dd487503d12037fae76a6e01ab76ee51b0385ae4
IkerromoMontilivi <a2409865@institutmontilivi.cat>
2024-11-03 00:34:03 +0100

    ex 2

---
+++ ex02/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ex01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaracio de variables
+            int cont;
+            string numero;
+            StreamReader sr = new StreamReader("C:\\Users\\Iker\\source\\repos\\Activitat 1.5\\ex02\\NUMEROS.TXT");
+
+            //Introduccio de valors
+            cont = 0;
+            numero = sr.ReadLine();
+
+            //Algorisme / calcul
+            while (numero != null)
+            {
+                cont++;
+                numero = sr.ReadLine();
+            }
+            //Mostrar resultats
+            Console.WriteLine($"Hi havien {cont} valors al fitxer");
+        }
+    }
+}
+
