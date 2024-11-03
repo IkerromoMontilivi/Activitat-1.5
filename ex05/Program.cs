9d04e574a375a3978635efcc893ab61a77098b47
IkerromoMontilivi <a2409865@institutmontilivi.cat>
2024-11-03 01:23:20 +0100

    ex5 i 6

---
+++ ex05/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ex05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaracio de variables
+            double numero, sumaNumeros, cont, mitjana;
+
+            //Introduccio de valors
+            Console.Write("Introduiex un numero: ");
+            numero = Convert.ToDouble(Console.ReadLine());
+            sumaNumeros = numero;
+            cont = 0;
+
+            //Algorisme / calcul
+            while (numero != 0)
+            {
+                Console.Write("Introdueix un altre valor: ");
+                numero = Convert.ToDouble(Console.ReadLine());
+                sumaNumeros = sumaNumeros + numero;
+                cont++;
+            }
+
+            mitjana = sumaNumeros / cont;
+
+            //Mostrar resultats
+            Console.WriteLine($"La mitjana es: {mitjana}");
+        }
+    }
+}
---
+++ ex06/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ex06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaracio de variables
+            double sumaNumeros, cont, mitjana;
+            string numero;
+
+            StreamReader sr = new StreamReader("C:\\Users\\Iker\\source\\repos\\Activitat 1.5\\ex06\\NUMEROS2.TXT");
+
+            //Introduccio de valors
+            numero = sr.ReadLine();
+            sumaNumeros = Convert.ToDouble(numero);
+            cont = 0;
+
+            //Algorisme / calcul
+            while (numero != null)
+            {
+                sumaNumeros = sumaNumeros + Convert.ToDouble(numero);
+                cont++;
+                numero = sr.ReadLine();
+            }
+
+            mitjana = sumaNumeros / cont;
+
+            //Mostrar resultats
+            Console.WriteLine($"La mitjana es: {mitjana}");
+        }
+    }
+}
