804cdeea8f2f14b586dc7fc4605b4d57467a0759
IkerromoMontilivi <a2409865@institutmontilivi.cat>
2024-11-03 02:01:53 +0100

    ex9

---
+++ ex09/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ex09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, cont, divisor, rdivisio;
+
+            Console.Write("Escriu un numero: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+            cont = 0;
+            divisor = 10;
+            rdivisio = -1;
+
+            while (rdivisio != 0)
+            {
+                rdivisio = numero / divisor;
+                cont++;
+                divisor = divisor * 10;
+            }
+
+            Console.WriteLine($"El numero té {cont} xifres");
+
+        }
+    }
+}
