f49b607ee352dd91561596a6aeb8e0d9c90a622f
IkerromoMontilivi <a2409865@institutmontilivi.cat>
2024-11-03 01:30:47 +0100

    ex8

---
+++ ex08/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ex08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numMajor, numMenor, numero;
+            string linea;
+
+            StreamReader sr = new StreamReader("C:\\Users\\Iker\\source\\repos\\Activitat 1.5\\ex04\\NUMEROS.TXT");
+
+            linea = sr.ReadLine();
+            numero = Convert.ToInt32(linea);
+            numMajor = numero;
+            numMenor = numero;
+
+            while (linea != null)
+            {       
+                if (numero > numMajor)
+                    numMajor = numero;
+                else if (numero < numMenor)
+                    numMenor = numero;
+
+                linea = sr.ReadLine();
+                numero = Convert.ToInt32(linea);
+            }
+
+            Console.WriteLine($"El valor mes gran es {numMajor} i el mes petit es {numMenor}");
+        }
+    }
+}
